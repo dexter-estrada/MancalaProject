86530a9742773515b98eefb2eeb2ebeed0482d18
LinYonEn <samuel.lam992@gmail.com>
2021-04-23 10:28:22 -0700

    updated UndoButton.java

--- src/UndoButton.java
+++ src/UndoButton.java
@@ -1,7 +1,62 @@
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * Description
+ * Constructs an "undo" button that allows 3 uses per move
  *
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
 public class UndoButton {
+    public UndoButton() {
+        JButton undo = new JButton("undo");
+        undo.addActionListener(event -> {
+            if (checkCounter() == MAXUSAGE) {
+                message();
+            } else {
+                iterateCounter();
+                //reset MancalaFrame
+            }
+        });
+    }
+
+    /*
+     *  Returns counter value
+     *  @param none
+     */
+    private int checkCounter() {
+        return counter;
+    }
+
+    /*
+     *  Returns counter to 0
+     *  @param none
+     */
+    public void resetCounter() {
+        counter = 0;
+    }
+
+    /*
+     *  Iterates counter forward by one
+     *  @param none
+     */
+    private void iterateCounter() {
+        counter++;
+    }
+
+    /*
+     *  Creates a pop informing the player that the maximum amount of
+     *  undo usages has been met and please select a pit
+     *  @param none
+     */
+    private void message() {
+        String text = "Maximum uses of the undo button have been reached. Please select a pit.";
+        String title = "Maximum Uses Reached";
+        JFrame popup = new JFrame();
+        JOptionPane.showMessageDialog(popup, text, title, JOptionPane.WARNING_MESSAGE);
+    }
+
+    private int counter = 0;
+    private final int MAXUSAGE = 3;
+
 }
