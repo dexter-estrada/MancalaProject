8686015994ca00ac6a2acd0d12ed9bb508cc9805
THANH LE <thanhlevan0203@gmail.com>
2021-04-23 11:48:35 -0700

    update PitButtons class

---
+++ src/PitButtons.java
@@ -0,0 +1,19 @@
+public class PitButtons {
+    private int indexPitButton;
+
+    /**
+     * Construct the PitButtons class with the indexPit given
+     * @param i - indexPit
+     */
+    public PitButtons(int index) {
+        indexPitButton = index;
+    }
+
+    /**
+     * Get the index button of Pit
+     * @return - index pit
+     */
+    public int getIndexPit() {
+        return indexPitButton;
+    }
+}
