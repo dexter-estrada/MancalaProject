381a8d9ce99d45d3f4f30a2889ceb660e5325de0
LinYonEn <samuel.lam992@gmail.com>
2021-04-23 10:15:49 -0700

    ToDo: add functions

--- src/StyleButton.java
+++ src/StyleButton.java
@@ -1,10 +1,8 @@
-import java.awt.*;
 import javax.swing.*;
 
 /**
  * Description
  * Creates a popup that allows the player to select the style of board
- *
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
 public class StyleButton {
--- src/UndoButton.java
+++ src/UndoButton.java
@@ -1,10 +1,8 @@
-import java.awt.*;
 import javax.swing.*;
 
 /**
  * Description
  * Constructs an "undo" button that allows 3 uses per move
- *
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
 public class UndoButton {
