523a8f8b5c22dd8c3106f8123f3817654e27cc5c
LinYonEn <samuel.lam992@gmail.com>
2021-04-30 12:42:21 -0700

    might need to update setPits. all else good to go

--- src/StoneSelection.java
+++ src/StoneSelection.java
@@ -1,7 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Description
  *
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
-public class StoneSelection {
+public class StoneSelection extends DataModel{
+    StoneSelection() {
+    JFrame popup = new JFrame();
+    Object[] text = {"3 stones", "4 stones"};
+    int choice = JOptionPane.showOptionDialog(popup, "Please select the amount of starting stones per pit", "Stone Selection", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, text, text[0]);
+    setStones(choice);
+}
+    void setStones(int number) {
+        int stones = number + 3;
+        setPits(stones);
+    }
 }
