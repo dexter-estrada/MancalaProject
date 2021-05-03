9ff5d4e827bdbf6e1be6f7b9901a78a02e80facf
Dexter Estrada <dexter.estrada@sjsu.edu>
2021-05-02 20:55:56 -0700

    Fixed StyleBoardGame

--- src/StyleBoardGame.java
+++ src/StyleBoardGame.java
@@ -7,26 +7,29 @@ import java.awt.*;
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
 public class StyleBoardGame extends JButton {
-    public MancalaFrame mancalaFrame;
+    public JFrame mancalaFrame;
     public DataModel dataModel;
     public  PitButtons[] pitButtons;
 
-    StyleBoardGame() {
-
+    StyleBoardGame(JFrame mancalaFrame, PitButtons[] pitButtons) {
+        this.mancalaFrame = mancalaFrame;
+        this.pitButtons = pitButtons;
         //this.pitButtons = pitButtons;
+        setText("Style Board Button");
+        addActionListener(e -> {
+            JFrame popup = new JFrame();
+            Object[] text = {"Day Style", "Night Style"};
+            int choice = JOptionPane.showOptionDialog(popup, "Please select a board style", "Style Selection", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, text, text[0]);
 
-        JFrame popup = new JFrame();
-        Object[] text = {"Day Style", "Night Style"};
-        int choice = JOptionPane.showOptionDialog(popup, "Please select a board style", "Style Selection", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, text, text[0]);
-
-        if (choice == 0) {
-            setDayStyle(mancalaFrame, pitButtons);
-        } else if (choice == 1) {
-            setNightStyle(mancalaFrame, pitButtons);
-        }
+            if (choice == 0) {
+                setDayStyle();
+            } else if (choice == 1) {
+                setNightStyle();
+            }
+        });
     }
 
-    void setDayStyle(MancalaFrame mancalaFrame, PitButtons[] pitButtons) {
+    void setDayStyle() {
         mancalaFrame.setBackground(Color.YELLOW);
         for (int i = 0; i < pitButtons.length; i++) {
             pitButtons[i].setBackground(Color.RED);
@@ -34,7 +37,7 @@ public class StyleBoardGame extends JButton {
         }
     }
 
-    void setNightStyle(MancalaFrame mancalaFrame, PitButtons[] pitButtons) {
+    void setNightStyle() {
         mancalaFrame.setBackground(Color.BLACK);
         for (PitButtons button : pitButtons) {
             button.setBackground(Color.BLUE);
