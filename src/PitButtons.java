9cf73b249b775db6e7ec5899eb742df6183ef9f3
THANH LE <thanhlevan0203@gmail.com>
2021-05-06 17:25:40 -0700

    deleted getPitValue()

--- src/PitButtons.java
+++ src/PitButtons.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  *
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
-public class PitButtons extends JButton implements ChangeListener {
+public class PitButtons extends JButton {
     private static final int PIT_WIDTH = 100;
     private static final int PIT_HEIGHT = 100;
     private int numStones;
@@ -25,15 +25,6 @@ public class PitButtons extends JButton implements ChangeListener {
      * Construct the PitButtons class with no parameter
      */
     public PitButtons() {
-
-    }
-
-    public PitButtons (DataModel dataModel, char pitSide, int pitNumber) {
-        this.dataModel = dataModel;
-        this.pitSide = pitSide;
-        this.pitNumber = pitNumber;
-
-        numStones = dataModel.getPitValue(pitSide, pitNumber);
 
     }
 
@@ -79,63 +70,6 @@ public class PitButtons extends JButton implements ChangeListener {
      */
     public String toString() {
         return namePitButton + "-" + indexPitButton;
-    }
-
-
-    /**
-     * Paint the shapes of stones inside the PitButtons
-     * @param g
-     */
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.GRAY);
-
-        for (Shape s: getPitStoneIcon(numStones)) {
-            g2.draw(s);
-            g2.fill(s);
-        }
-
-    }
-
-
-    /**
-     * Returns the list of stones's shape
-     * @param numStones
-     * @return
-     */
-    public Shape [] getPitStoneIcon(int numStones) {
-        if (numStones == 0) {
-            Rectangle2D stoneIcon = new Rectangle2D.Double(0, 0, 0, 0);
-
-            return new Shape[] {stoneIcon};
-        }
-
-        ArrayList<Rectangle2D.Double> iconList = new ArrayList<Rectangle2D.Double>();
-        int dimension = (int) (Math.sqrt(numStones) + 1);
-        int width = PIT_WIDTH/(dimension) - 10;
-        int height = PIT_HEIGHT/(dimension) - 10;
-
-        for (int i=0; i<numStones; i++) {
-            int xModulo = i%dimension;
-            int yModulo = i%dimension;
-
-            int x = xModulo*(width+4) + 10;
-            int y = yModulo*(height+4) + 5;
-
-            iconList.add(new Rectangle2D.Double(x, y, width, height));
-        }
-
-        Rectangle2D.Double [] rectangelList = new Rectangle2D.Double[iconList.size()];
-        rectangelList = iconList.toArray(rectangelList);
-
-        return rectangelList;
-
-    }
-
-
-    public void stateChanged(ChangeEvent e) {
-        numStones = dataModel.getPitValue(pitSide, pitNumber);
     }
 
 
