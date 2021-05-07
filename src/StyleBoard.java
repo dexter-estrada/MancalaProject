151b708d95f9f560b7f141e10b0c6a1ed60bf866
THANH LE <thanhlevan0203@gmail.com>
2021-05-06 18:14:33 -0700

    java doc for styleboard interface

--- src/StyleBoard.java
+++ src/StyleBoard.java
@@ -1,6 +1,5 @@
 /**
- * Description:
- *
+ * Description: Created to concrete strategies: setDayStyle and setNightStlye
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
 public interface StyleBoard {
