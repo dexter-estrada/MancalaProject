0bc6cf4711f2e184593855ef96a052b18910abcf
THANH LE <thanhlevan0203@gmail.com>
2021-05-06 17:04:59 -0700

    added StyleBoard interface

--- src/StyleBoard.java
+++ src/StyleBoard.java
@@ -1,3 +1,8 @@
+/**
+ * Description:
+ *
+ * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
+ */
 public interface StyleBoard {
     public void setDayStyle();
     public void setNightStyle();
