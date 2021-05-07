1474186f94564fc548283b864da21b33072b6d80
THANH LE <thanhlevan0203@gmail.com>
2021-05-06 18:34:21 -0700

    done javadoc StoneSelection class

--- src/StoneSelection.java
+++ src/StoneSelection.java
@@ -3,14 +3,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Description
+ * Description: Create the stone selections which has 3 or 4 stones
  *
  * @author Legendary: Thanh Le (thanh.le01@sjsu.edu), Samuel Lam (samuel.lam@sjsu.edu), Dexter Estrada (dexter.estrada@sjsu.edu)
  */
 public class StoneSelection extends JButton {
-    private JButton threeStonesButton;
-    private JButton fourStonesButton;
-    private JButton selectStoneButton;
     private int numStones;
     private MancalaFrame pitStones;
 
@@ -25,12 +22,9 @@ public class StoneSelection extends JButton {
             int choice = JOptionPane.showOptionDialog(popup, "Please select the amount stones in the Pit", "Stone Selection", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, text, text[0]);
             if (choice == 0) {
                 dataModel.setStones(3);
-                //numStones = 3;
-                //pitStones.add(numStones);
+
             } else if (choice == 1){
                 dataModel.setStones(4);
-                //numStones = 4;
-                //pitStones.add(numStones);
             }
         });
 
@@ -38,7 +32,7 @@ public class StoneSelection extends JButton {
 
     /**
      * Gets number stones in the Pit
-     * @return
+     * @return - number of stones
      */
     public int getNumStones() {
         return numStones;
@@ -46,7 +40,7 @@ public class StoneSelection extends JButton {
 
     /**
      * Gets stone button form MancalaFrame class
-     * @return
+     * @return - number of stones in a pit of each side
      */
     public JButton getPitStones() {
         return pitStones.getStoneButton();
